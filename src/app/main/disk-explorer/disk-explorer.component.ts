07f318704f597340a961dfd967b7b60f9640c83f
DevWurm <devwurm@devwurm.net>
2016-10-17 15:46:19 +0200

    Fixing code style issues

--- src/app/main/disk-explorer/disk-explorer.component.ts
+++ src/app/main/disk-explorer/disk-explorer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-disk-explorer',
+  selector: 'em-disk-explorer',
   templateUrl: './disk-explorer.component.html',
   styleUrls: ['./disk-explorer.component.css']
 })
