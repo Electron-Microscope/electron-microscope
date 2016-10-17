b6fd2e17c3819ebf55412d7d853705c94243ad86
DevWurm <devwurm@devwurm.net>
2016-10-17 17:31:15 +0200

    Fixing cpu-load component test

--- src/app/main/dashboard/cpu-load/cpu-load.component.spec.ts
+++ src/app/main/dashboard/cpu-load/cpu-load.component.spec.ts
@@ -2,10 +2,11 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { CpuLoadComponent } from './cpu-load.component';
+import { CpuLoadService } from './shared/cpu-load/cpu-load.service';
 
 describe('Component: CpuLoad', () => {
   it('should create an instance', () => {
-    let component = new CpuLoadComponent();
+    let component = new CpuLoadComponent(new CpuLoadService());
     expect(component).toBeTruthy();
   });
 });
