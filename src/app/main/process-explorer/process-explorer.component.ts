4690e14f0bc2070ad26b1780c58b26aa1b89892b
DevWurm <devwurm@devwurm.net>
2016-10-20 10:02:03 +0200

    Adding process-explorer view
    The view shows the running processes in a table view

--- src/app/main/process-explorer/process-explorer.component.ts
+++ src/app/main/process-explorer/process-explorer.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ColumnMode, TableColumn, TableOptions } from 'angular2-data-table';
+import { ProcessInformation, ProcessesService } from './shared/processes/processes.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'em-process-explorer',
   templateUrl: './process-explorer.component.html',
-  styleUrls: ['./process-explorer.component.css']
+  styleUrls: ['process-explorer.component.scss'],
+  providers: [
+    ProcessesService
+  ]
 })
-export class ProcessExplorerComponent implements OnInit {
+export class ProcessExplorerComponent implements OnInit, OnDestroy {
+  private subscriptions: Array<Subscription> = [];
 
-  constructor() { }
+  private interval = 5;
+
+  private rows: Array<ProcessInformation> = [];
+
+  private options = new TableOptions({
+    columnMode: ColumnMode.force,
+    headerHeight: 50,
+    footerHeight: 50,
+    rowHeight: 'auto',
+    columns: [
+      new TableColumn({ name: 'Pid' }),
+      new TableColumn({ name: 'Name' }),
+      new TableColumn({ name: 'Cpu', comparator: this.sorter.bind(this) }),
+      new TableColumn({ name: 'Memory', comparator: this.sorter.bind(this) }),
+    ]
+  });
+
+  constructor(private processes: ProcessesService) { }
 
   ngOnInit() {
+    this.subscriptions.push(this.processes.getProcessesInInterval(this.interval).subscribe(data => {
+      this.rows = data;
+    }));
   }
 
+  sorter() {}
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+  }
 }
