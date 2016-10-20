10e861456cc74a813f795d303954d3353fe8c7e6
DevWurm <devwurm@devwurm.net>
2016-10-20 10:01:27 +0200

    Updating processes-service
    The service now uses a more powerfull and stable library to receive
    process information

--- src/app/main/process-explorer/shared/processes/processes.service.ts
+++ src/app/main/process-explorer/shared/processes/processes.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
-const lookup: Function = require('ps-node').lookup;
+const lookup: Function = require('current-processes').get;
 import { Observable } from 'rxjs';
 
 export class ProcessInformation {
   pid: string;
-  command: string;
-  arguments: string;
+  name: string;
   cpu: number;
   memory: number;
 
-  constructor(pid: string, command: string, args: string) {
+  constructor(pid: string, name: string, cpu: number, memory: number) {
     this.pid = pid;
-    this.command = command;
-    this.arguments = args;
+    this.name = name;
+    this.cpu = cpu;
+    this.memory = memory;
   }
 }
 
@@ -25,16 +25,15 @@ export class ProcessesService {
     return Observable.interval(interval * 1000).flatMap(this.getProcesses);
   }
 
-  getProcesses() : Promise<Array<ProcessInformation>> {
+  getProcesses(): Promise<Array<ProcessInformation>> {
     // get the list of all running processes
     return new Promise((resolve, reject) => {
-      lookup({psargs: 'aux'}, (err, res) => {
-        if (err) { reject(err); return;}
+      lookup((err, res) => {
+        if (err) { reject(err); return; }
 
-        console.log(res);
-        resolve(res.map(currProcess => new ProcessInformation(currProcess.pid, currProcess.command, currProcess.arguments)));
-      })
-    })
+        resolve(res.map(currProcess => new ProcessInformation(currProcess.pid, currProcess.name, currProcess.cpu, currProcess.mem.usage)));
+      });
+    });
   }
 
 }
