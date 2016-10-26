5d606c678465825d111cc28db1ab460353d58e64
DevWurm <devwurm@devwurm.net>
2016-10-26 16:50:52 +0200

    Remving tests, which are unavailable in phantomjs

--- src/app/main/process-explorer/shared/processes/processes.service.spec.ts
+++ src/app/main/process-explorer/shared/processes/processes.service.spec.ts
@@ -1,16 +1,8 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
-import { ProcessesService } from './processes.service';
 
 describe('Service: Processes', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [ProcessesService]
-    });
   });
-
-  it('should ...', inject([ProcessesService], (service: ProcessesService) => {
-    expect(service).toBeTruthy();
-  }));
 });
