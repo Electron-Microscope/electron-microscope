eb2bc76df17d7aeceb9fdcc025e63fbbb9ad49fd
Garde, Ronny <ronny.garde@saxsys.de>
2016-10-28 17:11:12 +0200

    implementing disk explorer module: - donut chart (summarizes after 10 entries) - entry listing with navigation function

--- src/app/main/disk-explorer/disk-explorer.component.ts
+++ src/app/main/disk-explorer/disk-explorer.component.ts
@@ -3,6 +3,7 @@ import { ChartComponent } from 'angular2-highcharts';
 import { ViewChild } from '@angular/core/src/metadata/di';
 import { DiskExplorerService } from './shared/disk-explorer.service';
 import { resolve } from 'path';
+import { map } from 'rxjs/operator/map';
 
 @Component({
   selector: 'app-disk-explorer',
@@ -14,11 +15,14 @@ export class DiskExplorerComponent implements OnInit, AfterViewInit {
   @ViewChild(ChartComponent)
   private chart: ChartComponent;
 
-  private currentPath = process.cwd();
+  private colorPalette = ['#FF0000', '#0000FF', '#E040FB', '#7C4DFF', '#536DFE', '#448AFF', '#40C4FF', '#18FFFF', '#64FFDA',
+   '#69F0AE'];
+  private defaultColor = '#006600';
+
+  private currentPath = resolve(process.cwd(), 'src');
   private allFiles: Array<{name: string, size: number, color: string, directory:boolean}> = [];
   private options: HighchartsOptions = {
     chart: {
-
       plotBackgroundColor: null,
       plotBorderWidth: null,
       plotShadow: false,
@@ -29,7 +33,7 @@ export class DiskExplorerComponent implements OnInit, AfterViewInit {
       align: 'center',
       verticalAlign: 'middle',
       y: 0,
-      x: -30
+      x: 0
     },
     credits: {
       enabled: false
@@ -61,47 +65,57 @@ export class DiskExplorerComponent implements OnInit, AfterViewInit {
     this.showDir(this.currentPath);
 
     window.onresize = () => {
-      this.chart.chart.setSize(window.innerWidth - 500, window.innerHeight);
+      this.chart.chart.setSize((window.innerWidth - 500)*0.6, window.innerHeight*0.6);
     };
   }
 
   private showDir(dir:string) {
     const sizes = this.diskExplorerService.getEntries(dir);
 
+
     sizes.then((sizes) => {
-      sizes.sort(function(a, b) {
-        return a.size > b.size ? b.size : a.size;
-        //return a.name > b.name;
+      const sorted = sizes.sort(function(a, b) {
+        if(a.size == b.size) return 0;
+
+        return a.size < b.size ? 1 : -1;
+      }).map((entry, entryIndex) => {
+        entry.color = entryIndex < this.colorPalette.length ? this.colorPalette[entryIndex] : this.defaultColor;
+        return entry;
       });
 
-      this.allFiles.push({name: '..', size: 0, color: 0, directory:true});
-      sizes.forEach(size => {
-        this.allFiles.push(size);
+      this.allFiles = sorted;
 
+      const first = sorted.slice(0,9);
+      const other = sorted.slice(10).reduce((acc, curr) => {acc.size += curr.size; return acc}, {name: 'Other', size: 0, directory: true, color: this.defaultColor});
+
+      const chartEntries = first.concat(other);
+
+      let totalSize = Math.round(chartEntries.reduce((acc, curr) => acc + curr.size, 0)/1024/1024 * 100) / 100;
+      let totalSizeString = totalSize+" MB";
+
+      chartEntries.forEach(size => {
         this.chart.chart.series[0].addPoint({
           name: size.name,
           y: size.size,
           color: size.color
         });
-      })
+      });
+
+      this.chart.chart.setTitle({text:totalSizeString});
     });
+
+
   }
 
   private changeDir(newDir: string) {
     this.currentPath = resolve(this.currentPath, newDir);
 
     // clear
-    //this.chart.chart.series[0].data = [];
     this.chart.chart.series[0].setData([], true,true,true);
     this.allFiles = [];
 
-    this.showDir(this.currentPath);
-
-
-    console.log("CHANGED");
-
     // refresh
-
+    this.showDir(this.currentPath);
   }
 
 }
--- src/app/main/disk-explorer/shared/disk-explorer.service.ts
+++ src/app/main/disk-explorer/shared/disk-explorer.service.ts
@@ -1,40 +1,13 @@
 import { Injectable } from '@angular/core';
 import { readdir, stat } from 'fs';
 import { resolve as pathResolve } from "path";
-import { isArray } from 'util';
 
 @Injectable()
 export class DiskExplorerService {
 
-  //private colorPalette = ['#FF5252', '#FF4081', '#E040FB', '#7C4DFF', '#536DFE', '#448AFF', '#40C4FF', '#18FFFF', '#64FFDA',
-  // '#69F0AE', '#B2FF59', '#EEFF41', '#FFFF00', '#FFD740', '#FFAB40', '#FF6E40'];
-  private colorPalette = [];
-    private defaultColor = '#006600';
-
   constructor() { }
 
-  getColor():String {
-    if(this.colorPalette.length > 0) {
-      return this.colorPalette.pop();
-    } else {
-      //return this.defaultColor;
-      return this.colorPalette.push('#' + (function co(lor){   return (lor +=
-          [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)])
-        && (lor.length == 6) ?  lor : co(lor); })(''));
-    }
-  }
-
-  initColors() {
-    for (let i = 0; i < 30; i++) {
-      this.colorPalette.push('#' + (function co(lor){   return (lor +=
-          [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)])
-        && (lor.length == 6) ?  lor : co(lor); })(''));
-    }
-  }
-
   public getEntries(path: string): Promise<{name: string, size: number, color: string, directory:boolean}[]> {
-    this.initColors();
-
     return new Promise((resolve, reject) => {
       readdir(path, (err, entries: string[]) => {
         if (err) {
@@ -46,7 +19,7 @@ export class DiskExplorerService {
       })
     })
       .then(entries => {
-        return Promise.all(entries.map(entry => {
+        return Promise.all(entries.map((entry, entryIndex) => {
             return new Promise((resolve, reject) => {
               stat(pathResolve(path, entry), (err, entryStat) => {
                 if (err) {
@@ -57,31 +30,20 @@ export class DiskExplorerService {
                 resolve(entryStat);
               })
             })
-              .then(entryStat => {
+              .then((entryStat: {isDirectory: Function, size: number}) => {
                 if (entryStat.isDirectory()) {
-                  if (entry == "node_modules") {
-                    return {
-                      name: entry,
-                      size: 0,
-                      color: '#000000',
-                      directory: true
-                    };
-                  } else {
                    return this.getEntries(pathResolve(path, entry))
                       .then(sizes => {
                         return {
                           name: entry,
                           size: sizes.reduce((acc, curr) => acc + curr.size, 0),
-                          color: this.getColor(),
                           directory: true
                         };
                       });
-                  }
                 } else {
                   return {
                     name: entry,
                     size: entryStat.size,
-                    color: this.getColor(),
                     directory: false
                   };
                 }
