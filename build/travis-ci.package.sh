991005807ae65851347200d7db741c1b473f1223
DevWurm <devwurm@devwurm.net>
2016-11-04 15:18:43 +0100

    Using the correct mac packaging script

--- build/travis-ci.package.sh
+++ build/travis-ci.package.sh
@@ -5,7 +5,7 @@ then
   echo "Building distribution packages"
   TAG=$(echo $TRAVIS_TAG | sed 's/v//g')
   npm run build:prod
-  npm run package:linux && npm run build:mac
+  npm run package:linux && npm run package:mac
 else
   echo "Omitting packaging"
 fi
